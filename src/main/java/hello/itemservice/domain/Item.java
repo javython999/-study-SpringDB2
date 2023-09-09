e52e33d5f095922a8ef2949d43443c3c703c31f4
javython999 <infinity2544@gmail.com>
2023-09-09 18:22:20 +0900

    JPA apply

--- src/main/java/hello/itemservice/domain/Item.java
+++ src/main/java/hello/itemservice/domain/Item.java
@@ -2,11 +2,17 @@ package hello.itemservice.domain;
 
 import lombok.Data;
 
-@Data
-public class Item {
+import javax.persistence.*;
 
+@Data
+@Entity
+//@Table(name = "item") 매핑되는 테이블 지정 (자바 클래스명과 동일하면 생략 가능)
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "item_name", length = 10)
     private String itemName;
     private Integer price;
     private Integer quantity;
